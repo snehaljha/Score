f867307c28bfeabdf31b6d442fb464bb52920072
snehaljha <snehaljha08@gmail.com>
2021-09-05 05:38:26 -0700

    added InkWell padding

--- lib/Pages/menu.dart
+++ lib/Pages/menu.dart
@@ -10,38 +10,41 @@ class Menu extends StatelessWidget {
         Image.asset("assets/images/menu_img.jpg"),
         Container(
           margin: const EdgeInsets.only(top: 10.0),
-          padding: const EdgeInsets.all(padding),
           child: InkWell(
-            child: Row(
-              children: [
-                Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 10.0),
-                  child: Text("Favorites"),
-                )
-              ],
+            child: Container(
+              padding: const EdgeInsets.all(padding),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0),
+                    child: Text("Favorites"),
+                  )
+                ],
+              ),
             ),
           ),
         ),
         Container(
-          padding: const EdgeInsets.all(padding),
           child: InkWell(
-            child: Row(
-              children: [
-                Icon(Icons.tv),
-                Container(
-                  margin: const EdgeInsets.only(left: 10.0),
-                  child: Text("Matches"),
-                )
-              ],
+            child: Container(
+              padding: const EdgeInsets.all(padding),
+              child: Row(
+                children: [
+                  Icon(Icons.tv),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0),
+                    child: Text("Matches"),
+                  )
+                ],
+              ),
             ),
           ),
         ),
         Container(
-          padding: const EdgeInsets.all(padding),
           child: InkWell(
             onTap: () {
               Navigator.push(
@@ -49,14 +52,17 @@ class Menu extends StatelessWidget {
                 MaterialPageRoute(builder: (context) => Explore()),
               );
             },
-            child: Row(
-              children: [
-                Icon(Icons.explore),
-                Container(
-                  margin: const EdgeInsets.only(left: 10.0),
-                  child: Text("Explore"),
-                )
-              ],
+            child: Container(
+              padding: const EdgeInsets.all(padding),
+              child: Row(
+                children: [
+                  Icon(Icons.explore),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10.0),
+                    child: Text("Explore"),
+                  )
+                ],
+              ),
             ),
           ),
         )
