73f3f0cf1657ea758271ed3d8ccc177c7672ab6d
snehaljha <sj185226@ncr.com>
2021-09-14 14:35:12 +0530

    fixed drawer functionality

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -1,31 +1,27 @@
+import 'package:Score/Pages/LiveMatchesView.dart';
 import 'package:flutter/material.dart';
 
 import 'Pages/explore.dart';
 import 'Pages/home.dart';
 
 class Navigation {
+
   static int currentPage = -1;
 
   static void switchTo(int page, context) {
-    // if (page == currentPage) return;
-    // if (page == -11) {
-    //   Navigator.pop(context);
-    // } else if (currentPage != 0) {
-    Navigator.push(context, push(page, context)!);
-    // } else {
-    //   Navigator.pop(context);
-    //   Navigator.push(context, push(page, context)!);
-    // }
-    // currentPage = page;
+    if(page == currentPage) {
+      Navigator.pop(context);
+    }
+    Navigator.pushReplacement(context, getPage(page, context)!);
+    currentPage = page;
   }
 
-  static MaterialPageRoute? push(int page, context) {
-    if (page == 1) {
-      MaterialPageRoute(builder: (context) => HomePage());
-    } else if (page == 2) {
-      null;
-    } else if (page == 3)
-      return MaterialPageRoute(builder: (context) => Explore());
+  static MaterialPageRoute? getPage(int page, context) {
+    switch(page) {
+      case 1: return MaterialPageRoute(builder: (context) => HomePage());
+      case 2: return MaterialPageRoute(builder: (context) => LiveMatchesView());
+      case 3: return MaterialPageRoute(builder: (context) => Explore());
+    }
     return null;
   }
 }
